29f3dee0f76c79d1fec646db04d53a6706a03f26
John Le <73327543+9-5@users.noreply.github.com>
2023-11-13 21:49:53 -0500

    Update bookmarks.js

---
+++ home/bookmarks.js
@@ -0,0 +1,45 @@
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/privacy", url: "https://reddit.com/r/overwatch" },
+      {
+        name: "/r/engineering",
+        url: "https://reddit.com/r/engineering",
+      },
+      {
+        name: "/r/python",
+        url: "https://reddit.com/r/python",
+      },
+      { name: "/r/linux", url: "https://reddit.com/r/linux" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "LinkedIn", url: "https://linkedin.com" },
+    ],
+  },
+];
