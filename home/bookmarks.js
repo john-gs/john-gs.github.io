cc3b41e021e043d9f903a502fca1b1a3aa2b717d
John <john@johnle.org>
2023-12-07 16:11:53 -0500

    Update bookmarks.js

--- home/bookmarks.js
+++ home/bookmarks.js
@@ -2,44 +2,36 @@ const bookmarks = [
   {
     title: "Daily",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      {name: "c0mputers.org", url: "https://c0mputers.org"},
+      {name: "John Le", url: "https://johnle.org" },
+      {name: "GitHub", url: "https://github.com"},
+      {name: "Drive", url: "https://drive.google.com" },
     ],
   },
   {
     title: "Media",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
+      {name: "YouTube", url: "https://youtube.com"},
+      {name: "Flixtor", url: "https://flixtorz.to"},
+      {name: "Crunchyroll", url: "https://crunchyroll.com"},
+      {name: "Amazon Prime", url: "https://www.amazon.com/Amazon-Video"},
     ],
   },
   {
     title: "Reddit",
     links: [
-      { name: "/r/privacy", url: "https://reddit.com/r/overwatch" },
-      {
-        name: "/r/engineering",
-        url: "https://reddit.com/r/engineering",
-      },
-      {
-        name: "/r/python",
-        url: "https://reddit.com/r/python",
-      },
-      { name: "/r/linux", url: "https://reddit.com/r/linux" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
+      {name: "/r/privacy", url: "https://reddit.com/r/privacy"},
+      {name: "/r/engineering", url: "https://reddit.com/r/engineering"},
+      {name: "/r/python", url: "https://reddit.com/r/python"},
+      {name: "/r/linux", url: "https://reddit.com/r/linux"},
+      {name: "/r/news", url: "https://reddit.com/r/news" },
     ],
   },
   {
     title: "Social",
     links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "LinkedIn", url: "https://linkedin.com" },
+      {name: "Twitter", url: "https://twitter.com"},
+      {name: "LinkedIn", url: "https://linkedin.com"},
     ],
   },
 ];
